34f593277303ce35d026219d10df7a466b766f4c
root <33547792+Ryangom@users.noreply.github.com>
2022-07-04 14:07:29 +0600

    cleaning

--- Fontend/src/app/app.module.ts
+++ Fontend/src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './component/Font/home/home.component';
-import { AllUsersComponent } from './component/Font/all-users/all-users.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AllUsersComponent
   ],
   imports: [
     BrowserModule,
